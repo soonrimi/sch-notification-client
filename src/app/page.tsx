291ae2ee45ce2f44b4ce6242ba5915fc2dd5a4a1
KJH <skystadium@naver.com>
2025-08-08 21:52:29 +0900

    FE250808

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,90 +1,74 @@
+"use client";
 import Image from "next/image"
 import styles from "./page.module.css"
+import { TextField, Select ,MenuItem, Button, Stack} from "@mui/material"
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>가지마</ol>
+    <div style={{
+      height: '100vh'
+    }}>
+    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    <TextField id="filled-basic" label="Filled" variant="filled" />
+    <TextField id="standard-basic" label="Standard" variant="standard" />
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <Select value="1" >
+      <MenuItem value="1">메뉴1</MenuItem>
+      <MenuItem>메뉴2</MenuItem>
+      <MenuItem>메뉴3</MenuItem>
+    </Select>
+      
+      <Stack direction="row" gap="12px" p="12px" overflow="scroll" maxHeight="600px">
+        <button
+          style={{
+            border: '1px solid blue',
+            backgroundColor: 'white',
+            padding: '8px',
+            color: 'blue',
+            fontSize: '12px'
+          }}
+        >일반적인 버튼</button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+        <Button variant="outlined">MUI 버튼</Button>
+      </Stack>
+      
+      <Card variant="outlined" sx={{
+        margin: '12px',
+        padding: '12px'
+      }}>content</Card> 
+   </div>
   )
 }
