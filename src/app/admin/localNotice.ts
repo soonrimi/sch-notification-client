5edcbb753106c487b7dbf3cd432c64ce6993832b
SkyStadium <141819843+SkyStadium@users.noreply.github.com>
2025-08-22 20:45:55 +0900

    Update localNotice.ts
    빌드 테스트 문제 해결중..

--- src/app/admin/localNotice.ts
+++ src/app/admin/localNotice.ts
@@ -1,6 +1,6 @@
 'use client';
 
-export type Category = '전체' | '학교' | '대학' | '학년' | '채용' | '활동' | '홍보';
+export type Category = ⏎··|·'전체'⏎··|·'학교'⏎··|·'대학'⏎··|·'학년'⏎··|·'채용'⏎··|·'활동'⏎·;
 
 export type LocalAttachment = { name: string };
 export type LocalNotice = {
