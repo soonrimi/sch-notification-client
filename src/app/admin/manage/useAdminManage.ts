abe0f2d14b64d08804930c806e3d01e32491971d
iubns <iubns@naver.com>
2025-10-26 22:28:17 +0900

    refactoring: hook으로 상태관리 변경

--- src/app/admin/manage/useAdminManage.ts
+++ src/app/admin/manage/useAdminManage.ts
@@ -1,12 +1,17 @@
-import { AdminControllerService, AdminUserResponse, Department } from '@/api';
+import { useEffect } from 'react';
 import { atom, useAtom } from 'jotai';
-import { useEffect, useState } from 'react';
+import { AdminControllerService, AdminUserResponse, Department } from '@/api';
 
+const selectedUserAtom = atom<AdminUserResponse | null>(null);
 const adminUserListAtom = atom<AdminUserResponse[]>([]);
+const departmentListAtom = atom<Department[]>([]);
+const modeAtom = atom<'view' | 'add' | 'edit'>('view');
 
 export default function useAdminManage() {
+  const [mode, setMode] = useAtom(modeAtom);
+  const [selectedUser, setSelectedUser] = useAtom(selectedUserAtom);
   const [adminUserList, setAdminUserList] = useAtom(adminUserListAtom);
-  const [departmentList, setDepartmentList] = useState<Department[]>([]);
+  const [departmentList, setDepartmentList] = useAtom(departmentListAtom);
 
   useEffect(() => {
     if (adminUserList.length === 0) {
@@ -31,54 +36,49 @@ export default function useAdminManage() {
     updatedUser: AdminUserResponse,
     requestPassword: string
   ) {
-    // AdminUpdateRequest로 변환
-    try {
-      const data = await AdminControllerService.updateAdmin(updatedUser.id, {
-        name: updatedUser.name,
-        affiliation: updatedUser.affiliation,
-        registerPassword: requestPassword,
-        password: undefined, // 비밀번호 변경이 필요할 때만 전달
-        categories: updatedUser.categories ?? [],
-        departmentIds: updatedUser.departments
-          ? updatedUser.departments.map((d) => d.id ?? 0)
-          : [],
-        grades: updatedUser.grades ?? [],
-      });
-      setAdminUserList((prevList) =>
-        prevList.map((user) => (user.id === data.id ? data : user))
-      );
-      return data;
-    } catch (error) {
-      console.error('Failed to update admin user:', error);
-    }
+    const data = await AdminControllerService.updateAdmin(updatedUser.id, {
+      name: updatedUser.name,
+      affiliation: updatedUser.affiliation,
+      registerPassword: requestPassword,
+      password: undefined, // 비밀번호 변경이 필요할 때만 전달
+      categories: updatedUser.categories ?? [],
+      departmentIds: updatedUser.departments
+        ? updatedUser.departments.map((d) => d.id ?? 0)
+        : [],
+      grades: updatedUser.grades ?? [],
+    });
+    setAdminUserList((prevList) =>
+      prevList.map((user) => (user.id === data.id ? data : user))
+    );
+    return data;
   }
 
   async function createAdminUser(
     newUser: AdminUserResponse & { password: string; registerPassword: string }
   ) {
-    try {
-      await AdminControllerService.register({
-        userId: newUser.userId,
-        password: newUser.password,
-        name: newUser.name,
-        affiliation: newUser.affiliation,
-        registerPassword: newUser.registerPassword,
-        categories: newUser.categories ?? [],
-        departmentIds: newUser.departments
-          ? newUser.departments.map((d) => d.id ?? 0)
-          : [],
-        grades: newUser.grades ?? [],
-      });
-    } catch (error) {
-      console.error('Failed to create admin user:', error);
-    }
+    await AdminControllerService.register({
+      userId: newUser.userId,
+      password: newUser.password,
+      name: newUser.name,
+      affiliation: newUser.affiliation,
+      registerPassword: newUser.registerPassword,
+      categories: newUser.categories ?? [],
+      departmentIds: newUser.departments
+        ? newUser.departments.map((d) => d.id ?? 0)
+        : [],
+      grades: newUser.grades ?? [],
+    });
   }
 
   return {
+    selectedUser,
+    setSelectedUser,
+    departmentList,
     adminUserList,
     fetchAdminUsers,
     createAdminUser,
     updateAdminUser,
-    departmentList,
+    mode,
+    setMode,
   };
 }
