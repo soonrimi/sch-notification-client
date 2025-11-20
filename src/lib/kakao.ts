7d70aad28f6f0b698a011b70e34ee1a1394dddd9
HongJeongMin <150657014+HongJeongMin0214@users.noreply.github.com>
2025-11-20 23:44:31 +0900

    ESLint 빌드 에러 수정
    ESLint 빌드 에러 수정: any 타입을 KakaoShareOptions 인터페이스로 변경
    카카오 공유 옵션의 타입을 명시적으로 정의

--- src/lib/kakao.ts
+++ src/lib/kakao.ts
@@ -1,10 +1,30 @@
+export interface KakaoShareOptions {
+  objectType: string;
+  content: {
+    title: string;
+    description: string;
+    imageUrl: string;
+    link: {
+      mobileWebUrl: string;
+      webUrl: string;
+    };
+  };
+  buttons: Array<{
+    title: string;
+    link: {
+      mobileWebUrl: string;
+      webUrl: string;
+    };
+  }>;
+}
+
 declare global {
   interface Window {
     Kakao?: {
       init: (appKey: string) => void;
       isInitialized: () => boolean;
       Share: {
-        sendDefault: (options: any) => void;
+        sendDefault: (options: KakaoShareOptions) => void;
       };
     };
   }
